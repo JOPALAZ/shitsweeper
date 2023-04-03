51914be06d0e259e46a3bc6190ac379b490d9ae1
JOPALAZ <102656898+JOPALAZ@users.noreply.github.com>
2023-04-03 13:08:34 +0200

    заменил ShitField на gameMap

--- shitswepper/GameField.cpp
+++ shitswepper/GameField.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 std::map< std::string, sf::Texture*>* GameField::LoadTextures(std::string path)
 {
-    if (textures!=nullptr) {
+    if (textures != nullptr) {
         for (auto& k : *textures) {
             if (k.second != nullptr) {
                 k.second->~Texture();
@@ -15,25 +15,25 @@ std::map< std::string, sf::Texture*>* GameField::LoadTextures(std::string path)
     }
     sf::Texture* buf = new sf::Texture;
     std::map<std::string, sf::Texture*>* exhaust = new std::map<std::string, sf::Texture*>;
-    for (short i{ -1 }; i < 9; ++i) 
-        {
-            buf->loadFromFile(path + "N" + std::to_string(i) + ".png");
-            exhaust->insert(std::make_pair("N" + std::to_string(i), buf));
-            buf = nullptr;
-            buf = new sf::Texture;
-        }
-        buf->loadFromFile(path + "UNKNOWN.png");
-        exhaust->insert(std::make_pair("UNKNOWN", buf));
+    for (short i{ -1 }; i < 9; ++i)
+    {
+        buf->loadFromFile(path + "N" + std::to_string(i) + ".png");
+        exhaust->insert(std::make_pair("N" + std::to_string(i), buf));
         buf = nullptr;
         buf = new sf::Texture;
-        buf->loadFromFile(path + "QUESTION.png");
-        exhaust->insert(std::make_pair("QUESTION", buf));
-        buf = nullptr;
-        buf = new sf::Texture;
-        buf->loadFromFile(path + "QUESTIONUS.png");
-        exhaust->insert(std::make_pair("QUESTIONUS", buf));
-        buf = nullptr;
-        return exhaust;
+    }
+    buf->loadFromFile(path + "UNKNOWN.png");
+    exhaust->insert(std::make_pair("UNKNOWN", buf));
+    buf = nullptr;
+    buf = new sf::Texture;
+    buf->loadFromFile(path + "QUESTION.png");
+    exhaust->insert(std::make_pair("QUESTION", buf));
+    buf = nullptr;
+    buf = new sf::Texture;
+    buf->loadFromFile(path + "QUESTIONUS.png");
+    exhaust->insert(std::make_pair("QUESTIONUS", buf));
+    buf = nullptr;
+    return exhaust;
 }
 void GameField::loadSoundTrack()
 {
@@ -42,64 +42,63 @@ void GameField::loadSoundTrack()
     SoundTrack.setLoop(true);
     SoundTrack.play();
     if (path == "BARBARIKI")
-        SoundTrack.setVolume(6.f); // íåâåðîÿòíî ãðîìêî, âûøå 20 ñòàâèòü âîîáùå íåëüçÿ
+        SoundTrack.setVolume(6.f); // невероятно громко, выше 20 ставить вообще нельзя
     else if (path == "SEREGA PIRAT")
-        SoundTrack.setVolume(0.f); 
-    //Íà ìóçûêå ìåíÿ ó÷èëè ÷òî ìóçûêà  ó÷èò ñàìûì ëó÷øèì ÷óâñòâàì ëþáâè òàì, äðóæáå. Âîñïèòûâàåò â íàñ âêóñ, âåñåëèò. 
-    //Ñåð¸ãà ïîêàçûâàåò íà ñâîåì ïðèìåðå ÷òî ìóçûêà åù¸ è ìîæåò çàñòàâèòü ÷åëîâåêà çàäóìàòüñÿ î ñìûñëå æèçíè,
-    //îêîí÷àòåëüíî óáåäèâ ÷òî ëó÷øå óìåðåòü, ÷åì ñëóøàòü ïèðàòà
-    //Ñïàñèáî, Ñåðåãà, ÿ âêëþ÷èë ýòó ïåñíè ïðè äðóçüÿõ è âñåì îíà íàñòîëüêî ïîíðàâèëàñü, ÷òî ìåíÿ ïîãëàäèëè áèòîé è òåïåðü ÿ îòäûõàþ â áîëüíèöå
-    //ß ñëóøàë ýòîò òðåê íà óëèöå è ó áîìæà ëîïíóëà áàðàáàííàÿ ïåðåïîíêà è åãî ïîâåçëè â áîëüíèöó. Ñïàñèáî, Ñåðåãà, òåïåðü ó áîìæà åñòü õîòü ìåñòî ïðîæèâàíèÿ
-    //×åëîâåê, êîòîðûé óçíàë î ñåðåãå ïèðàòå  áûë õóäîæíèêîì. Îí ïîñëóøàë åå è âäîõíîâèëñÿ èäååé. Ýòèì õóäîæíèêîì áûë àäîëüô ãèòëåð
-    //Äóìàë ÷òî ñëóõ—ýòî äàð, ïîòîì âêëþ÷èë òðýê ñåðåãè è ïîíÿë, ÷òî ýòî íå òàê.
-    //Òðåê íàñòîëüêî âçðûâíîé ÷òî ó ìåíÿ âçîðâàëèñü ïåðåïîíêè
-    //Ñåð¸ãà, ïîñëå ïðîñëóøèâàíèå òâîåãî òðåêà, âðà÷è âåðíóëè ðàêàâóþ îïóõîëü îáðàòíî
-    //Ñåð¸ãà ñïàñèáî çà òâî¸ òâîð÷åñòâî, âêëþ÷èë êîãäà ìàìà áûëà äîìà, âñåãäà áûëî èíòåðåñíî êàê æèâ¸òñÿ íà óëèöå!
-    //Ñïàñèáî âàì Ñåðãåé! Âû ñïàñëè ìîþ æèçíü. Íà äíÿõ ìåíÿ îêðóæèëà òîëïà ãîïíèêîâ, è âîò êîãäà ÿ óæå áûë ãîòîâ îùóòèòü áîòèíîê íà ñâîåì ëèöå,
-    //ÿ óñëûøàë ýòó ïåñíþ. Ãîïíèêè ñðàçó æå îòñòàëè ìåíÿ è ïîøëè èçáèâàòü êîãî òî äðóãîãî!
-    else
-        SoundTrack.setVolume(50.f);
+        SoundTrack.setVolume(0.f);
+    //На музыке меня учили что музыка  учит самым лучшим чувствам любви там, дружбе. Воспитывает в нас вкус, веселит. 
+    //Серёга показывает на своем примере что музыка ещё и может заставить человека задуматься о смысле жизни,
+    //окончательно убедив что лучше умереть, чем слушать пирата
+    //Спасибо, Серега, я включил эту песни при друзьях и всем она настолько понравилась, что меня погладили битой и теперь я отдыхаю в больнице
+    //Я слушал этот трек на улице и у бомжа лопнула барабанная перепонка и его повезли в больницу. Спасибо, Серега, теперь у бомжа есть хоть место проживания
+    //Человек, который узнал о сереге пирате  был художником. Он послушал ее и вдохновился идеей. Этим художником был адольф гитлер
+    //Думал что слух—это дар, потом включил трэк сереги и понял, что это не так.
+    //Трек настолько взрывной что у меня взорвались перепонки
+    //Серёга, после прослушивание твоего трека, врачи вернули ракавую опухоль обратно
+    //Серёга спасибо за твоё творчество, включил когда мама была дома, всегда было интересно как живётся на улице!
+    //Спасибо вам Сергей! Вы спасли мою жизнь. На днях меня окружила толпа гопников, и вот когда я уже был готов ощутить ботинок на своем лице,
+    //я услышал эту песню. Гопники сразу же отстали меня и пошли избивать кого то другого!    else
+    else  SoundTrack.setVolume(50.f);
 }
 void GameField::unloadSoundTrack()
 {
-    if(SoundTrack.Playing)
+    if (SoundTrack.Playing)
         SoundTrack.stop();
     if (WinLoseSound.Playing)
         WinLoseSound.stop();
 }
-void GameField::MakeGameField(std::string path,int difficulty){
-    headerTexture.loadFromFile(path+"header.png");
+void GameField::MakeGameField(std::string path, int difficulty) {
+    headerTexture.loadFromFile(path + "header.png");
     textures = LoadTextures(path);
     header.setTexture(headerTexture, true);
     header.setPosition(sf::Vector2f(0, 0));
-    scaleX = WINDOW_RES.first / (float)header.getTexture()->getSize().x*0.6f;
+    scaleX = WINDOW_RES.first / (float)header.getTexture()->getSize().x * 0.6f;
     scaleY = (WINDOW_RES.second * MARGIN_SCALE) / header.getTexture()->getSize().y;
     header.setScale(scaleX, scaleY);
-    float minefieldScaleX,minefieldScaleY;
+    float minefieldScaleX, minefieldScaleY;
     if (WINDOW_RES.first > WINDOW_RES.second) {
         minefieldScaleX = 1.f;
         minefieldScaleY = WINDOW_RES.second / (float)WINDOW_RES.first;
     }
     else {
-        minefieldScaleY=1.f;
-        minefieldScaleX= WINDOW_RES.first / (float)WINDOW_RES.second;
+        minefieldScaleY = 1.f;
+        minefieldScaleX = WINDOW_RES.first / (float)WINDOW_RES.second;
     }
-    ShitField.deleteUnique();
-    ShitField.create(difficulty*minefieldScaleY, difficulty * minefieldScaleX,
+    gameMap.deleteUnique();
+    gameMap.create(difficulty * minefieldScaleY, difficulty * minefieldScaleX,
         textures, std::make_pair(WINDOW_RES.second * MARGIN_SCALE, 0),
         std::make_pair(WINDOW_RES.second, WINDOW_RES.first));
     if (!Font.loadFromFile("Font.TTF")) { std::cout << "FONT WASN'T LOADED\n"; }
     bombAmount.setFont(Font);
     screenSpaceAllocatedForText = WINDOW_RES.first - headerTexture.getSize().x * scaleX;
-    bombAmountString = "BOMBS LEFT: " + std::to_string(ShitField.getBombAmount());
+    bombAmountString = "BOMBS LEFT: " + std::to_string(gameMap.getBombAmount());
     bombAmount.setString(bombAmountString);
-    this->charSizeHeight = screenSpaceAllocatedForText*(1-2*marginFromHeader)*2 / bombAmountString.size();
+    this->charSizeHeight = screenSpaceAllocatedForText * (1 - 2 * marginFromHeader) * 2 / bombAmountString.size();
     while (bombAmount.getLocalBounds().width <= WINDOW_RES.first * 0.32f && bombAmount.getLocalBounds().height <= WINDOW_RES.first * MARGIN_SCALE * 0.8f)
     {
         charSizeHeight++;
         bombAmount.setCharacterSize(charSizeHeight);
     }
-    while (bombAmount.getLocalBounds().width>WINDOW_RES.first*0.32f|| bombAmount.getLocalBounds().height > WINDOW_RES.first*MARGIN_SCALE*0.8f)
+    while (bombAmount.getLocalBounds().width > WINDOW_RES.first * 0.32f || bombAmount.getLocalBounds().height > WINDOW_RES.first * MARGIN_SCALE * 0.8f)
     {
         charSizeHeight--;
         bombAmount.setCharacterSize(charSizeHeight);
@@ -111,7 +110,7 @@ void GameField::MakeGameField(std::string path,int difficulty){
     bombAmount.setFillColor(sf::Color::White);
     loadSoundTrack();
     gameIsOn = true;
-    
+
 }
 void GameField::drawAllElements(sf::RenderWindow* window) {
     if (draw) {
@@ -120,10 +119,10 @@ void GameField::drawAllElements(sf::RenderWindow* window) {
             bombAmount.setString(bombAmountString);
             window->draw(header);
             window->draw(bombAmount);
-            ShitField.drawMap(window);
+            gameMap.drawMap(window);
 
         }
-        else if(drawGameMenu) {
+        else if (drawGameMenu) {
             menu.draw(window);
         }
     }
@@ -135,7 +134,7 @@ bool GameField::leftClickOnField(sf::RenderWindow* window)
     if (gameIsOn) {
         int x = sf::Mouse::getPosition(*window).x;
         int y = sf::Mouse::getPosition(*window).y;
-        if (!ShitField.leftClickOnMap(x, y))
+        if (!gameMap.leftClickOnMap(x, y))
         {
 
             bombAmountString = "YOU LOST";
@@ -155,16 +154,16 @@ bool GameField::leftClickOnField(sf::RenderWindow* window)
                 bombAmount.getLocalBounds().top + bombAmount.getLocalBounds().height / 2.0f);
             bombAmount.setPosition(WINDOW_RES.first * 0.8f, WINDOW_RES.second * MARGIN_SCALE / 2.f);
             bombAmount.setFillColor(sf::Color(128, 64, 48)); // KORICHNEVY 
-            ShitField.openAllMap();
+            gameMap.openAllMap();
             window->clear();
             drawAllElements(window);
             window->display();
             playWinLooseSound(false);
             gameIsOn = false;
-            
+
             return false;
         }
-        if (ShitField.checkWinState()) {
+        if (gameMap.checkWinState()) {
             bombAmountString = "YOU WON";
             bombAmount.setString(bombAmountString);
             while (bombAmount.getLocalBounds().width <= WINDOW_RES.first * 0.32f && bombAmount.getLocalBounds().height <= WINDOW_RES.first * MARGIN_SCALE * 0.8f)
@@ -182,7 +181,7 @@ bool GameField::leftClickOnField(sf::RenderWindow* window)
                 bombAmount.getLocalBounds().top + bombAmount.getLocalBounds().height / 2.0f);
             bombAmount.setPosition(WINDOW_RES.first * 0.8f, WINDOW_RES.second * MARGIN_SCALE / 2.f);
             bombAmount.setFillColor(sf::Color(9, 121, 105)); // ZELENIY
-            ShitField.openAllMap();
+            gameMap.openAllMap();
             window->clear();
             drawAllElements(window);
             window->display();
@@ -190,24 +189,24 @@ bool GameField::leftClickOnField(sf::RenderWindow* window)
             gameIsOn = false;
             return true;
         }
-        bombAmountString = "BOMBS LEFT: " + std::to_string(ShitField.getBombAmount());
+        bombAmountString = "BOMBS LEFT: " + std::to_string(gameMap.getBombAmount());
         return true;
     }
     else
     {
         this->path = menu.leftClick(window);
         if (this->path.size() != 0) {
-            MakeGameField(this->path+"\\",getDifficulty());
+            MakeGameField(this->path + "\\", getDifficulty());
         }
 
-    
-    
+
+
     }
     return false;
 }
 unsigned GameField::getDifficulty()
 {
-    std::ifstream file(path+"\\dif");
+    std::ifstream file(path + "\\dif");
     unsigned difficulty;
     file >> difficulty;
     return difficulty;
@@ -226,9 +225,9 @@ void GameField::rightClickOnField(sf::RenderWindow* window) {
     if (gameIsOn) {
         float x = sf::Mouse::getPosition(*window).x;
         float y = sf::Mouse::getPosition(*window).y;
-        ShitField.rightClickOnMap(x, y);
-        if (ShitField.getBombAmount() == 0) {
-            if (ShitField.checkWinState()) {
+        gameMap.rightClickOnMap(x, y);
+        if (gameMap.getBombAmount() == 0) {
+            if (gameMap.checkWinState()) {
                 bombAmountString = "YOU WON";
                 bombAmount.setString(bombAmountString);
                 while (bombAmount.getLocalBounds().width <= WINDOW_RES.first * 0.32f && bombAmount.getLocalBounds().height <= WINDOW_RES.first * MARGIN_SCALE * 0.8f)
@@ -246,7 +245,7 @@ void GameField::rightClickOnField(sf::RenderWindow* window) {
                     bombAmount.getLocalBounds().top + bombAmount.getLocalBounds().height / 2.0f);
                 bombAmount.setPosition(WINDOW_RES.first * 0.8f, WINDOW_RES.second * MARGIN_SCALE / 2.f);
                 bombAmount.setFillColor(sf::Color(9, 121, 105)); // ZELENIY
-                ShitField.openAllMap();
+                gameMap.openAllMap();
                 window->clear();
                 drawAllElements(window);
                 window->display();
@@ -255,17 +254,26 @@ void GameField::rightClickOnField(sf::RenderWindow* window) {
                 return;
             }
         }
-        bombAmountString = "BOMBS LEFT: " + std::to_string(ShitField.getBombAmount());
+        bombAmountString = "BOMBS LEFT: " + std::to_string(gameMap.getBombAmount());
     }
 }
 void GameField::drawMenu(sf::RenderWindow* window) {
     menu.draw(window);
 }
 void GameField::wait(unsigned msec) {
-    sf::sleep(sf::milliseconds(msec)); // ãåíèé)
+    sf::sleep(sf::milliseconds(msec)); // гений)
 }
 GameField::~GameField() {
-    if(textures)
-    textures->clear();
-    //îñòàëüíîå óäàëèòñÿ ñàìî
+    if (textures != nullptr) {
+        for (auto& k : *textures) {
+            if (k.second != nullptr) {
+                k.second->~Texture();
+                k.second = nullptr;
+            }
+        }
+        textures->clear();
+        delete textures;
+        textures = nullptr;
+    }
+    //остальное удалится само, 
 }
